63ce6346f33f469ff1c1df20eede897630ed9aa1
rrowell <2006834@student.acalanes.k12.ca.us>
2017-03-15 12:36:15 -0700

    driver

---
+++ connectfour/connect4Driver.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class connect4Driver here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class connect4Driver
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class connect4Driver
+     */
+    public connect4Driver()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
