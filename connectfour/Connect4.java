941f5091933ba0422ae76a2c00d6cc5336bd8414
rrowell <2006834@student.acalanes.k12.ca.us>
2017-03-21 13:02:21 -0700

    beans

--- connectfour/Connect4.java
+++ connectfour/Connect4.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner;
 /**
  * Write a description of class connect4Driver here.
  * 
@@ -8,10 +8,34 @@
 public class Connect4
 {
     // instance variables - replace the example below with your own
+    int numPlayers = 0;
+    boolean AI = false;
     public void main(){
         Board Connect4 = new Board();
         player player1 = new player("o", false);
-        player player2 = new player("x", true);
+        wantAI();
+        player player2 = new player("x", AI);
+    }
+public void wantAI(){
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter 1 for 1 player and 2 for 2 player");
+int numPlayers = sc.nextInt();
+if(numPlayers == 1){
+    AI = false;
+}
+else{
+    if(numPlayers == 2) {
+        AI = true;
+    }
+    else{
+        System.out.println("Please enter a valid number");
+        wantAI();
         
     }
+        
+    }
+}
+
+
+
 }
