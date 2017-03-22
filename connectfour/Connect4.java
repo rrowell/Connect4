2d375bd8380d8b4f06c179794d9173867eb2f989
danielajk99 <2005933@student.acalanes.k12.ca.us>
2017-03-22 13:18:28 -0700

    NUT

--- connectfour/Connect4.java
+++ connectfour/Connect4.java
@@ -10,31 +10,62 @@ public class Connect4
     // instance variables - replace the example below with your own
     int numPlayers = 0;
     boolean AI = false;
+    public int winner = 0;
     public void main(){
-        Board Connect4 = new Board();
+        Board gameboard = new Board();
         player player1 = new player("o", false);
         wantAI();
         player player2 = new player("x", AI);
-    }
-public void wantAI(){
-Scanner sc = new Scanner(System.in);
-System.out.println("Enter 1 for 1 player and 2 for 2 player");
-int numPlayers = sc.nextInt();
-if(numPlayers == 1){
-    AI = false;
-}
-else{
-    if(numPlayers == 2) {
-        AI = true;
-    }
-    else{
-        System.out.println("Please enter a valid number");
-        wantAI();
         
+        while(gameboard.check() == false){
+             Scanner sc = new Scanner(System.in);
+             System.out.println("Enter which column you want to drop (1-7)");
+             int col = sc.nextInt();
+             gameboard.drop(col, player1.symbol);
+             gameboard.printer();
+            if (gameboard.check()==true){
+                winner = 1;
+                break;
+                
+             }
+             if (AI == false){
+                 Scanner bc = new Scanner(System.in);
+                 System.out.println("Enter which column you want to drop (1-7)");
+                 col = bc.nextInt();
+                 gameboard.drop(col, player2.symbol);
+            }
+            if (AI == true){
+                gameboard.drop(player2.dropAI(), player2.symbol);
+            }
+            gameboard.printer();
+            winner = 2;
+        }
+        if (winner == 1){
+            System.out.println("Player 1 wins, congrats");
+        }
+        else{
+            System.out.println("Player 2 wins, congrats");
+        }
     }
-        
+    public void wantAI(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 1 for 1 player and 2 for 2 player");
+        int numPlayers = sc.nextInt();
+        if(numPlayers == 1){
+            AI = false;
+        }
+        else{
+            if(numPlayers == 2) {
+                AI = true;
+            }
+            else{
+                System.out.println("Please enter a valid number");
+                wantAI();
+                
+            }
+                
+            }
     }
-}
 
 
 
